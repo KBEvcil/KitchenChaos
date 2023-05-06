1b830c4a2f8b273b150467b5cd92f2b6b70166bd
KBEvcil <l1119096@std.yildiz.edu.tr>
2023-05-06 17:02:50 +0300

    Sync Pick up Objects

---
+++ Assets/GameAssets/Scripts/KitchenGameMultiplayer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class KitchenGameMultiplayer : NetworkBehaviour
+{
+    public static KitchenGameMultiplayer Instance { get; private set; }
+
+    [SerializeField]
+    private KitchenObjectListSO kitchenObjectListSO;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
+    {
+        int kitchenObjectSOIndex = GetKitchenObjectSOIndex(kitchenObjectSO);
+        SpawnKitchenObjectServerRpc(kitchenObjectSOIndex, kitchenObjectParent.GetNetworkObject());
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SpawnKitchenObjectServerRpc(int kitchenObjectSOIndex, NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+        KitchenObjectSO kitchenObjectSO = GetKitchenObjectSOFromIndex(kitchenObjectSOIndex);
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+
+        NetworkObject kitchenObjectNetworkObject = kitchenObjectTransform.GetComponent<NetworkObject>();
+        kitchenObjectNetworkObject.Spawn();
+
+        KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+
+        kitchenObjectParentNetworkObjectReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
+        IKitchenObjectParent kitchenObjectParent = kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
+        kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
+    }
+
+    private int GetKitchenObjectSOIndex(KitchenObjectSO kitchenObjectSO)
+    {
+        return kitchenObjectListSO.kitchenObjectSOList.IndexOf(kitchenObjectSO);
+    }
+
+    private KitchenObjectSO GetKitchenObjectSOFromIndex(int i)
+    {
+        return kitchenObjectListSO.kitchenObjectSOList[i];
+    }
+}
